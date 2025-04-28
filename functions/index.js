544ba2e8d28105b7378e797d0ce7383d848de8a7
DamienLove <119716433+DamienLove@users.noreply.github.com>
2025-04-28 02:29:30 -0600

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,33 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+require('dotenv').config();
+const functions = require('firebase-functions');
+const admin     = require('firebase-admin');
+const cors      = require('cors')({origin: true});
+
+admin.initializeApp();
+const db = admin.firestore();
+
+// POST /enqueueCommand  {action:"ahk", payload:{script:"MsgBox Hi!"}}
+exports.enqueueCommand = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    if (req.method !== 'POST') return res.status(405).send('Use POST');
+
+    if (req.get('x-api-key') !== process.env.API_SECRET)
+      return res.status(401).send('Bad key');
+
+    const body = req.body;
+    if (!body?.action) return res.status(400).send('Missing action');
+
+    const ref = await db.collection('commands').add({
+      ...body,
+      status: 'new',
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+    res.json({id: ref.id});
+  });
+});
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
